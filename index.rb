57ada78641e391c679f708365e58f2f6297077f7
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-05-22 20:35:09 +0700

    Display uptime on main dashboard.

--- index.rb
+++ index.rb
@@ -3,8 +3,8 @@ require 'sinatra'
 require 'mongo'
 require 'haml'
 
-def get_status(host_id)
-  check = @db.collection('checks').find({ 'host_id' => host_id }).sort(['timestamp', 'descending']).first
+def get_last_state(host)
+  check = @db.collection('checks').find({ 'host_id' => host['_id'] }).sort(['timestamp', 'descending']).first
   return ['-', '-'] unless check
   status = check['status'] ? 'ok' : 'fail'
   [status, check['timestamp']]
@@ -14,6 +14,15 @@ def human_time(timestamp)
   Time.at(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 end
 
+def get_checks_result(host)
+  checks = {
+    :total => @db.collection('checks').find({ 'host_id' => host['_id'] }).count,
+    :failed => @db.collection('checks').find({ 'host_id' => host['_id'], 'status' => false }).count
+  }
+  checks[:uptime] = (checks[:total] - checks[:failed]) * 100 / checks[:total]
+  checks
+end
+
 before do
   @db = Mongo::Connection.new.db('checks')
 end
@@ -21,11 +30,12 @@ end
 get '/' do
   @hosts = []
   @db.collection('hosts').find.each do |host|
-    status, check_timestamp = get_status(host['_id'])
+    status, check_timestamp = get_last_state(host)
     @hosts << { 
       :ip => host['ip'],
       :host => host['host'],
       :status => status,
+      :uptime => get_checks_result(host)[:uptime],
       :time => '-' == check_timestamp ? '-' : human_time(check_timestamp),
     }
   end
@@ -40,10 +50,6 @@ end
 
 get '/details' do
   @host = @db.collection('hosts').find_one({ 'ip' => params[:ip] })
-  @checks = {
-    :total => @db.collection('checks').find({ 'host_id' => @host['_id'] }).count,
-    :failed => @db.collection('checks').find({ 'host_id' => @host['_id'], 'status' => false }).count
-  }
-  @checks[:uptime] = (@checks[:total] - @checks[:failed]) * 100 / @checks[:total]
+  @checks = get_checks_result(@host)
   haml :details, :format => :html5
 end
